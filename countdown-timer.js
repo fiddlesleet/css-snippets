135476ce055340d6cd10f181630ca1796b87b051
Hannah Smythe <chris.smythe11@gmail.com>
2019-05-02 16:50:30 -0400

    Update countdown-timer.js

--- countdown-timer.js
+++ countdown-timer.js
@@ -3,8 +3,8 @@
 
 <script>
 // Set the date we're counting down to
-function countdownDays() { 
-  var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+function countdownDays(date) { 
+  var countDownDate = new Date(date + " 23:59:59").getTime();
 
 	// Update the count down every 1 second
 	var x = setInterval(function() {
@@ -29,5 +29,5 @@ function countdownDays() {
 }, 1000);
 };
   
-countdownDays();
+countdownDays("May 15, 2019");
 </script>
